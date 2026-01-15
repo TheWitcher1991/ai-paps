07a1627158773515382a575c2ab92c744b82d0c7
Ashot Svazyan <ashot.svazyan222@gmail.com>
2026-01-15 18:55:23 +0300

    feat(core): init

---
+++ clients/packages/system/eslint.config.js
@@ -0,0 +1,4 @@
+import { config } from '@wcsc/eslint-config/base'
+
+/** @type {import('eslint').Linter.Config} */
+export default { config }
---
+++ clients/packages/types/index.ts
@@ -0,0 +1,137 @@
+import type { AxiosResponse, AxiosError } from 'axios'
+import React from 'react'
+import type { ObjectSchema } from 'valibot'
+
+declare const __brand: unique symbol
+
+export type Branded<T, UniqueKey extends string> = T & { __brand: UniqueKey }
+
+export type Nullable<T> = T | null
+
+export type EnumType<T> = T[keyof T]
+
+export type EmptyDictionary = Record<string, never>
+
+export type Dictionary<T = unknown> = Record<string, T>
+
+export type RequestResponse<T = unknown> = Promise<AxiosResponse<T>>
+
+export type RequestError = AxiosError
+
+export type PartialBy<T, K extends keyof T> = Omit<T, K> & Partial<Pick<T, K>>
+
+export type Expand<T> = T extends infer O ? { [K in keyof O]: O[K] } : never
+
+export type Timeout = ReturnType<typeof setTimeout>
+
+export type OrderDirection = 'ASC' | 'DESC'
+
+export type LogLevel = 'log' | 'warn' | 'error'
+
+export type MergeObjectEntries<T extends ObjectSchema<any, any>[]> =
+		UnionToIntersection<
+			T[number] extends ObjectSchema<infer R, any> ? R : never
+		>
+
+export type InjectProps<
+	Key extends string,
+	Value,
+	Extras extends Record<string, any> = {},
+> = {
+	[K in Key]: Value
+} & Extras
+
+export type OnUploadProgress = (
+	progress: number,
+	uploaded: number,
+	total: number,
+) => void
+
+export interface ErrorObject {
+	code: string
+	message: string
+	type: string
+}
+
+export type ErrorList = ErrorObject[]
+
+type ModelConfig<T extends string> = {
+	model: T
+	models: `${T}s`
+	infiniteModels: `infinite-${T}s`
+}
+
+export interface Paginated<RESULTS = [], META = Record<string, any>> {
+	count: number
+	pages: number
+	next: Nullable<string>
+	previous: Nullable<string>
+	meta: META
+	results: RESULTS[]
+}
+
+export interface PaginateQuery<
+		ORDERING extends string = string,
+	> extends PaginationPageSize {
+		query: string
+		ordering: ORDERING
+	}
+
+export interface PagesListResponse<T> {
+	pages: Paginated<T>[]
+	pageParams: number[]
+}
+
+export type UnionToIntersection<U> = (
+	U extends any ? (k: U) => void : never
+) extends (k: infer I) => void
+	? I
+	: never
+
+export type DetailedDivProps = React.DetailedHTMLProps<
+	React.ButtonHTMLAttributes<HTMLDivElement>,
+	HTMLDivElement
+>
+
+export type DetailedButtonProps = React.DetailedHTMLProps<
+	React.ButtonHTMLAttributes<HTMLButtonElement>,
+	HTMLButtonElement
+>
+
+export type DetailedInputProps = React.InputHTMLAttributes<HTMLInputElement>
+
+export type DetailedSelectProps = React.DetailedHTMLProps<
+	React.SelectHTMLAttributes<HTMLSelectElement>,
+	HTMLSelectElement
+>
+
+export type DetailedLabelProps = React.DetailedHTMLProps<
+	React.LabelHTMLAttributes<HTMLLabelElement>,
+	HTMLLabelElement
+>
+
+export type DetailedTextareaProps = React.DetailedHTMLProps<
+	React.TextareaHTMLAttributes<HTMLTextAreaElement>,
+	HTMLTextAreaElement
+>
+
+export type ModelListField<T, U extends Dictionary<any>> = {
+	count: number
+	loading: boolean
+	error: boolean
+	fetching?: boolean
+	list: T[]
+	filter: U
+	checked?: number[]
+}
+
+export type ModelListState<T, U extends Dictionary<any>> = {
+	setCount: (count: number) => void
+	setError: (error: boolean) => void
+	setLoading: (loading: boolean) => void
+	setFetching: (fetching: boolean) => void
+	setChecked: (checked: number[]) => void
+	setList: (list: T[]) => void
+	setFilter: (filter: U) => void
+	reset: () => void
+} & ModelListField<T, U>
